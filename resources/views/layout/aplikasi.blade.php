4fe3735edd7341c6459d800d731ed16081f21846
Rizkimwp <rizki.mwp404@gmail.com>
2023-07-28 11:33:22 +0700

    second commit

---
+++ resources/views/layout/aplikasi.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <title>Sewa Mobil</title>
+</head>
+<body>
+<header>
+
+
+</header>
+
+<main>
+    @yield('konten')
+</main>
+
+<footer></footer>
+</body>
+</html>
